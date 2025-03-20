d3fb6b350b23fccbaa23162740fcab5f1af99342
Daniel P.K <danilofletcher978@gmail.com>
2025-03-20 22:23:31 +0000

    calculator of course

---
+++ calculator.py
@@ -0,0 +1,11 @@
+#add subtract multiply divide 
+
+def add_numbers(num1, num2):
+    return (num1 + num2)
+num1 = int(input("Please input the first number: "))
+num2 = int(input("Please input the second number: "))
+add_numbers(num1, num2)
+
+#main
+#def calculator():
+    #pass
