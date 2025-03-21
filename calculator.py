0c2e75ac9fd32f448bb892cb6ff64d32881dd098
Daniel P.K <danilofletcher978@gmail.com>
2025-03-21 09:01:18 +0000

    calc v1.basic.

--- calculator.py
+++ calculator.py
@@ -1,11 +1,41 @@
 #add subtract multiply divide 
 
+
 def add_numbers(num1, num2):
-    return (num1 + num2)
-num1 = int(input("Please input the first number: "))
-num2 = int(input("Please input the second number: "))
-add_numbers(num1, num2)
+    print (num1 + num2)
+
+def subtract(num1, num2):
+    print(num1 - num2)
+
+
+def multiply(num1, num2):
+    print(num1 * num2)
+    
+def divide(num1, num2):
+    print(num1/num2)
+
 
 #main
-#def calculator():
-    #pass
+def calculator():
+    #running = True
+    print("Welcome to my calculator. select 1(add), 2(subtract), 3(multiply), 4(divide)")
+    
+    choice = int(input("Please choose a number to peform an operation: "))
+    
+    
+    if choice == 1:
+        add_numbers(num1, num2)
+    elif choice == 2:
+            subtract(num1, num2)
+    elif choice ==3: 
+            multiply(num1, num2)
+    elif choice == 4:
+        divide(num1, num2)
+        running = False
+    else:
+        print("Invalid Option ): ")
+
+num1 = int(input("Please input the first number: "))
+num2 = int(input("Please input the second number: "))
+
+calculator()
