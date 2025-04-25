cda62d39503c068bf2fa491be404db797d20c056
Daniel P.K <danilofletcher978@gmail.com>
2025-04-25 00:29:50 +0000

    best time to buy and sell stocks

--- interview_prep.py
+++ interview_prep.py
@@ -1,12 +1,26 @@
-nums = [2, 7, 11, 15]
-target = 9  
+#Problem Statement:
+'''You are given an array prices where prices[i] is the price of a stock on day i.
+You want to maximize profit by choosing one day to buy and a future day to sell.
+Return the maximum profit possible. If no profit exists, return 0.'''
 
-def two_sum(nums, target):
-    set = {}
-    for i, num in enumerate(nums):
-        complement = target - num
-        if complement in set:
-            return complement, num
-        set[num] = i
+#so the best day to sell would be the highest price and vice versa but we cant sell in the past so... yeah.  
+prices = [7, 1, 5, 3, 6, 4]
+# Buy at 1 (Day 2), sell at 6 (Day 5)
+# Profit = 5 
+
+def forex_trader(prices):
+    if len(prices) < 2:  # Edge case: Not enough days to trade
+        return 0
     
-print(two_sum(nums, 9))
+    min_price = float("inf")
+    max_profit = 0
+    
+    for price in prices:
+        if price < min_price:
+            min_price = price 
+            
+        current_profit = price - min_price
+        if current_profit > max_profit:
+            max_profit = current_profit
+    return max_profit
+print(forex_trader(prices))
